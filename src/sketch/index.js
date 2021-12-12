58c7557537d0fb8d2d04e8d02c1a04363e65bb14
Ray Weitzenberg <ray@mechaneyes.com>
2021-12-12 00:38:00 -0800

    color cycling works + uses image mask
    color cycling is working. it's slow though, and when i try to speed it
    up, the animation gets jaggy. not sure of the fix + target colors are
    random as of now

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,22 +1,38 @@
 export default function sketch(s) {
   let backgroundColor;
   let ewithin;
+  let emask;
   let ar;
+  let startColor = s.color(255, 255, 255);
+  let newColor = s.color(s.random(255), s.random(255), s.random(255), 126);
+  let amt = 0;
 
   s.preload = () => {
     ewithin = s.loadImage("assets/emoni-within.jpg");
+    emask = s.loadImage("assets/emoni-within-mask.png");
   };
 
   s.setup = () => {
+    s.frameRate(60);
     s.createCanvas(s.windowWidth, s.windowHeight);
     backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
 
     ar = ewithin.height / ewithin.width;
-    s.image(ewithin, 0, 0, s.windowWidth, s.windowWidth * ar);
+    ewithin.mask(emask);
   };
 
   s.draw = () => {
-    // s.background(backgroundColor);
+    // s.background(255, 255, 255);
+
+    amt += 0.01;
+    if (amt >= 1) {
+      amt = 0.0;
+      startColor = newColor;
+      newColor = s.color(s.random(255), s.random(255), s.random(255), 126);
+    }
+
+    s.tint(s.lerpColor(startColor, newColor, amt));
+    s.image(ewithin, 0, 0, s.windowWidth, s.windowWidth * ar);
   };
 
   s.mousePressed = () => {
