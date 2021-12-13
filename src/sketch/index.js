585bdefbe57729a89681f19a21ad9fbfb763e59a
Ray Weitzenberg <ray@mechaneyes.com>
2021-12-12 17:11:27 -0800

    color cycling on grayscale elements
    had to take the image to grayscale as the original colors were fucking up the tints combined with them + lotta bullshit photoshop editing to get masks going

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,79 +1,112 @@
 export default function sketch(s) {
-  let ewithin;
-  let emask;
+  let img_arms;
+  let img_red;
+  let img_yellow;
+  let img_blue;
+  let mask_red;
+  let mask_yellow;
+  let mask_blue;
   let ar;
-  let startColor = s.color(255, 255, 255);
-  let newColor = s.color(s.random(255), s.random(255), s.random(255), 126);
-  let amt = 0;
-  let colour = 0;
+  let colourRed = 5;
+  let colourYellow = 60;
+  let colourBlue = 240;
+  const cycleSpeed = 2;
 
   s.preload = () => {
-    // ewithin = s.loadImage("assets/emoni-within.jpg");
-    // emask = s.loadImage("assets/emoni-within-mask.png");
+    img_arms = s.loadImage("assets/emoni-img-arms.png");
 
-    ewithin = s.loadImage(
-      "https://live.staticflickr.com/65535/51740197162_7e08a7cc41_b.jpg"
-    );
-    emask = s.loadImage(
-      "https://live.staticflickr.com/65535/51741661129_c7aae754a3_o.png"
-    );
+    img_red = s.loadImage("assets/emoni-img-red.png");
+    mask_red = s.loadImage("assets/emoni-mask-red.png");
+
+    img_blue = s.loadImage("assets/emoni-img-blue.png");
+    mask_blue = s.loadImage("assets/emoni-mask-blue.png");
+
+    img_yellow = s.loadImage("assets/emoni-img-yellow.png");
+    mask_yellow = s.loadImage("assets/emoni-mask-yellow.png");
   };
 
   s.setup = () => {
     s.frameRate(60);
     s.createCanvas(s.windowWidth, s.windowHeight);
 
-    ewithin.mask(emask);
+    img_red.mask(mask_red);
+    img_blue.mask(mask_blue);
+    img_yellow.mask(mask_yellow);
   };
 
   s.draw = () => {
     setSizeRatio();
 
-    amt += 0.01;
-    if (amt >= 1) {
-      amt = 0.0;
-      startColor = newColor;
-      newColor = s.color(s.random(255), s.random(255), s.random(255));
-    }
-    s.tint(s.lerpColor(startColor, newColor, amt));
-
-    // Cycling through the color wheel using hue,
-    // saturation and brightness
+    // Color cyclers
     //
-    // s.colorMode(s.HSB);
-    // s.tint(colour, 100, 100);
-    // colour += 0.2;
-    // if (colour > 360) {
-    //   colour = 0;
-    // }
+    colourRed += cycleSpeed;
+    if (colourRed > 360) {
+      colourRed = 0;
+    }
+    colourYellow += cycleSpeed;
+    if (colourYellow > 360) {
+      colourYellow = 0;
+    }
+    colourBlue += cycleSpeed;
+    if (colourBlue > 360) {
+      colourBlue = 0;
+    }
   };
 
   s.windowResized = () => {
-    setTimeout(() => {
-      s.background(255, 255, 255);
-    }, 1);
     s.resizeCanvas(s.windowWidth, s.windowHeight);
 
-    setSizeRatio();
-  };
+    setTimeout(() => {
+      s.colorMode(s.RGB);
+      s.background(255);
+    }, 1);
 
-  s.mousePressed = () => {
-    newColor = s.color(s.random(255), s.random(255), s.random(255));
+    setSizeRatio();
   };
 
   // Set size and aspect ratio
   //
   let setSizeRatio = () => {
     if (s.windowWidth >= s.windowHeight) {
-      ar = ewithin.height / ewithin.width;
+      ar = img_arms.height / img_arms.width;
     } else {
-      ar = ewithin.width / ewithin.height;
+      ar = img_arms.width / img_arms.height;
     }
 
     if (s.windowWidth >= s.windowHeight) {
-      s.image(ewithin, 0, 0, s.windowHeight / ar, s.windowHeight);
+      s.colorMode(s.RGB);
+      s.tint(255);
+      s.image(img_arms, 0, 0, s.windowHeight / ar, s.windowHeight);
+
+      // Set tints to cycle through the color wheel
+      // using hue, saturation and brightness
+      //
+      s.colorMode(s.HSB);
+      s.tint(colourRed, 100, 100);
+      s.image(img_red, 0, 0, s.windowHeight / ar, s.windowHeight);
+      s.tint(colourYellow, 100, 100);
+      s.image(img_yellow, 0, 0, s.windowHeight / ar, s.windowHeight);
+      s.tint(colourBlue, 100, 100);
+      s.image(img_blue, 0, 0, s.windowHeight / ar, s.windowHeight);
     } else {
-      s.image(ewithin, 0, 0, s.windowWidth, s.windowWidth / ar);
+      s.colorMode(s.RGB);
+      s.tint(255);
+      s.image(img_arms, 0, 0, s.windowWidth, s.windowWidth / ar);
+
+      s.colorMode(s.HSB);
+      s.tint(colourRed, 100, 100);
+      s.image(img_red, 0, 0, s.windowWidth, s.windowWidth / ar);
+      s.tint(colourYellow, 100, 100);
+      s.image(img_yellow, 0, 0, s.windowWidth, s.windowWidth / ar);
+      s.tint(colourBlue, 100, 100);
+      s.image(img_blue, 0, 0, s.windowWidth, s.windowWidth / ar);
     }
   };
+
+  // let colorCycler = (theColor) => {
+  //   theColor += cycleSpeed;
+  //   if (theColor > 360) {
+  //     theColor = 0;
+  //   }
+  // };
 }
